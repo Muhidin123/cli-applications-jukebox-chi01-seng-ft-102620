e7434c6b5a9d148ddc35760b14de73b8ec1def31
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-10-14 23:31:18 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -29,7 +29,7 @@ def play(songs)
   user_input = gets.strip
   if user_input.to_i >= 1 && user_input.to_i <= songs.length
     puts "Playing #{songs[user_input.to_i - 1]}"
-  elsif songs(include?)
+  elsif songs.include?(user_input)
 
 
 def exit_jukebox
